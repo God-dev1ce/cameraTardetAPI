9e1757974c6a54738de97d84ad0e7c8709013c75
雨辰 <577758548@qq.com>
2025-06-13 14:44:00 +0800

    modify：修改请求返回状态

--- utils/response.py
+++ utils/response.py
@@ -1,7 +1,22 @@
+from fastapi.responses import JSONResponse
 from schemas.response import ResponseModel
 
 def success_response(data=None, msg="操作成功"):
-    return ResponseModel(code=200, msg=msg, data=data)
+    return JSONResponse(
+        status_code=200,
+        content={
+            "code": 200,
+            "msg": msg,
+            "data": data
+        }
+    )
 
 def error_response(code=400, msg="操作失败"):
-    return ResponseModel(code=code, msg=msg)
+    return JSONResponse(
+        status_code=code,
+        content={
+            "code": code,
+            "msg": msg,
+            "data": None
+        }
+    )
